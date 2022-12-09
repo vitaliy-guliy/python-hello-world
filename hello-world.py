b9b5d9441025790de3b5960c236f06e6ef413870
Nick Boldt <nboldt@redhat.com>
2022-12-09 16:25:09 -0400

    chore: CRW-2619 fix lint issues (flake8)
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,7 @@
-#--------------------------------------------------------------------------------------------
+# -------------------------------------------------------------
 #  Copyright (c) Red Hat, Inc. All rights reserved.
-#  Licensed under the MIT License. See LICENSE in the project root for license information.
-#--------------------------------------------------------------------------------------------
+#  Licensed under the MIT License. See LICENSE in project root.
+# -------------------------------------------------------------
 
 # This program prints Hello, world!
 
