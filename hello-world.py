92a6d6aa9ee02452f305cee06735cac9c07d013d
Nick Boldt <nboldt@redhat.com>
2022-12-09 16:28:05 -0400

    add copyright date
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,7 @@
 # -------------------------------------------------------------
-#  Copyright (c) Red Hat, Inc. All rights reserved.
+#  Copyright (c) 2022 Red Hat, Inc. All rights reserved.
 #  Licensed under the MIT License. See LICENSE in project root.
 # -------------------------------------------------------------
-
 # This program prints Hello, world!
 
 str = 'world'
